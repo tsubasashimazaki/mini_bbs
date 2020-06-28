ccf725183697501b9cf96f208f3e0802a7b866e2
github tsubasashimazaki <github>
2020-06-28 12:14:53 +0900

    modified: フォーム送信時にエラー表示

--- join/index.php
+++ join/index.php
@@ -1,7 +1,28 @@
 <?php 
-if($_POST['name'] === ''){
-	echo '名前は必須です';
+// リクエスト送信された時にが空だったらエラー
+if(!empty($_POST)){
+
+	if($_POST['name'] === ''){
+		$error['name'] = 'blank';
+	}
+	if($_POST['email'] === ''){
+		$error['email'] = 'blank';
+	}
+	if(strlen($_POST['password']) <= 8){
+		$error['password'] = 'length';
+	}
+	if($_POST['password'] === ''){
+		$error['password'] = 'blank';
+	}
+	
+	if(empty($error)){
+		
+		// check.phpにジャンプする命令
+		header('Location: check.php');
+		exit();
+	}
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -26,14 +47,26 @@ if($_POST['name'] === ''){
 	<dl>
 		<dt>ニックネーム<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="name" size="35" maxlength="255" value="" />
+        	<input type="text" name="name" size="35" maxlength="255" value="<?php echo(htmlspecialchars($_POST['name'],ENT_QUOTES)); ?>" />
+			<?php if($error['name'] === 'blank'): ?>
+			<p class="error">＊ニックネームは必須です！</p>
+			<?php endif; ?>
 		</dd>
 		<dt>メールアドレス<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="email" size="35" maxlength="255" value="" />
+        	<input type="text" name="email" size="35" maxlength="255" value="<?php echo(htmlspecialchars($_POST['email'],ENT_QUOTES)); ?>" />
+			<?php if($error['email'] === 'blank'): ?>
+				<p class="error">*メールアドレスは必須です。</p>
+			<?php endif; ?>
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
-        	<input type="password" name="password" size="10" maxlength="20" value="" />
+        	<input type="password" name="password" size="10" maxlength="20" value="<?php echo (htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" />
+			<?php if($error['password'] === 'blank'): ?>
+			<p class="error">*パスワードは必須です。</p>
+			<?php endif; ?>
+			<?php if($error['password'] === 'length'): ?>
+			<p class="error">パスワードは8文字以上で入力してください。</p>
+			<?php endif; ?>
         </dd>
 		<dt>写真など</dt>
 		<dd>
