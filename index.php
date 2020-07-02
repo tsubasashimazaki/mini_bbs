38721f48a027209db4d2d59608753accc23e273f
github tsubasashimazaki <github>
2020-07-02 11:14:17 +0900

    modified: 投稿したデータをDBに保存+投稿データを一覧画面に表示

--- index.php
+++ index.php
@@ -12,7 +12,31 @@ if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) { //1時間後�
   header('Location:login.php');
   exit();
 }
+
+if(!empty($_POST)) {
+  if($_POST['message'] !== '') {
+    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
+    $message->execute(array(
+      $member['id'], // $_SESSION['id']と同じだがデータベースから取得した方が正確性が上がる
+      $_POST['message'], // 投稿したメッセージ
+    ));
+
+    header('Location:index.php'); // 投稿して再読み込みすると$_POSTが投稿データを持ち続けた状態になるので強制的に自身の画面を呼び出してリセットをかける
+  }
+}
+
+// 投稿を取得するプログラム(ユーザーが入力した値ではないのでqueryメソッド)
+/*
+m.name mはエイリアスでmembersテーブルのname(ニックネーム)
+m.picture mはエイリアスでmembersテーブルのpictureのパス名
+p.* 投稿された全てのデータ
+m.id=p.member_idでリレーション。　投稿者と投稿されるデータは1対多の関係になる為リレーションすること
+*/
+$posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC'); //mやpはテーブルのショートカット名
+
+
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -47,17 +71,19 @@ if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) { //1時間後�
       </div>
     </form>
 
+<?php foreach($posts as $post): //リレーションされている投稿データを一件ずつ表示 ?>
+
     <div class="msg">
-    <img src="member_picture" width="48" height="48" alt="" />
-    <p><span class="name">（）</span>[<a href="index.php?res=">Re</a>]</p>
-    <p class="day"><a href="view.php?id="></a>
+    <img src="member_picture/<?php echo(htmlspecialchars($post['picture'], ENT_QUOTES)); //echoしたものはファイル名のみなのでディレクトリも必要?>" width="48" height="48" alt="<?php echo(htmlspecialchars($post['name'], ENT_QUOTES)); ?>" />
+    <p><?php echo(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php echo(htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=">Re</a>]</p>
+    <p class="day"><a href="view.php?id="><?php echo(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
 <a href="view.php?id=">
 返信元のメッセージ</a>
 [<a href="delete.php?id="
 style="color: #F33;">削除</a>]
     </p>
     </div>
-
+<?php endforeach; ?>
 <ul class="paging">
 <li><a href="index.php?page=">前のページへ</a></li>
 <li><a href="index.php?page=">次のページへ</a></li>
